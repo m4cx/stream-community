6d099b49d8668c69222a750d3f439f01d619d649
m4cx <macgyverx83@gmail.com>
2021-12-19 13:25:28 +0100

    chore(ef-core-migrations): add migration classes for enlistments

---
+++ src/StreamCommunity.Persistence/Migrations/20211219122345_AddEnlistments.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StreamCommunity.Persistence.Migrations
+{
+    public partial class AddEnlistments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "tc");
+
+            migrationBuilder.CreateTable(
+                name: "Enlistments",
+                schema: "tc",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserName = table.Column<string>(type: "TEXT", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    State = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Enlistments", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Enlistments",
+                schema: "tc");
+        }
+    }
+}
