08877660d8f8571334c24f8f58717ac7d2389554
m4cx <macgyverx83@gmail.com>
2021-12-22 18:08:10 +0100

    tests(enlistment): add unit-tests

---
+++ test/StreamCommunity.Domain.Tests/EnlistmentTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using NUnit.Framework;
+using StreamCommunity.Domain.Exceptions;
+
+namespace StreamCommunity.Domain.Tests;
+
+[TestFixture]
+public class EnlistmentTest
+{
+    private Enlistment enlistment = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+        enlistment = new Enlistment("testUser", DateTime.Now);
+    }
+    
+    [Test]
+    public void Draw_StateIsActive_ThrowsEnlistmentException()
+    {
+        
+        enlistment.Draw();
+
+        Assert.Throws<EnlistmentException>(() => enlistment.Draw());
+    }
+
+    [Test]
+    public void Draw_StateIsClosed_ThrowsEnlistmentException()
+    {
+        enlistment.Draw();
+        enlistment.Close();
+        
+        Assert.Throws<EnlistmentException>(() => enlistment.Draw());
+    }
+
+    [Test]
+    public void Draw_StateIsOpen_StateIsSetToActive()
+    {
+        enlistment.Draw();
+        Assert.AreEqual(EnlistmentState.Active, enlistment.State);
+    }
+
+    [Test]
+    public void Close_StateIsOpen_ThrowsEnlistmentException()
+    {
+        Assert.Throws<EnlistmentException>(() => enlistment.Close());
+    }
+    
+    [Test]
+    public void Close_StateIsClosed_ThrowsEnlistmentException()
+    {
+        enlistment.Draw();
+        enlistment.Close();
+        Assert.Throws<EnlistmentException>(() => enlistment.Close());
+    }
+    
+    [Test]
+    public void Draw_StateIsActive_StateIsSetToClosed()
+    {
+        enlistment.Draw();
+        enlistment.Close();
+        Assert.AreEqual(EnlistmentState.Closed, enlistment.State);
+    }
+}
