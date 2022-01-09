ea995ad1db1590872ee74cc8852c1137b089caa7
m4cx <macgyverx83@gmail.com>
2022-01-09 12:16:39 +0100

    tests(viewer-games): add tests for get-enlistments request

--- test/StreamCommunity.Application.Tests/MockDbTestBase.cs
+++ test/StreamCommunity.Application.Tests/MockDbTestBase.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
 using StreamCommunity.Application.Persistence;
+using StreamCommunity.Domain;
 using StreamCommunity.Persistence;
 
 namespace StreamCommunity.Application.Tests;
@@ -16,7 +20,17 @@ public abstract class MockDbTestBase
         var dbContextOptions = new DbContextOptionsBuilder<StreamCommunityDbContext>()
             .UseInMemoryDatabase(Guid.NewGuid().ToString())
             .Options;
-        
+
         DbContext = new StreamCommunityDbContext(dbContextOptions);
+    }
+
+    protected async Task AddEnlistmentsAsync(IEnumerable<Enlistment> entities, CancellationToken cancellationToken = default)
+    {
+        foreach (var entity in entities)
+        {
+            DbContext.Enlistments.Add(entity);
+        }
+
+        await DbContext.SaveChangesAsync(cancellationToken);
     }
 }
---
+++ test/StreamCommunity.Application.Tests/ViewerGames/Enlistments/Handler/GetEnlistmentsRequestHandlerTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using StreamCommunity.Application.ViewerGames.Enlistments;
+using StreamCommunity.Application.ViewerGames.Enlistments.Handler;
+using StreamCommunity.Domain;
+
+namespace StreamCommunity.Application.Tests.ViewerGames.Enlistments.Handler;
+
+public class GetEnlistmentsRequestHandlerTest : MockDbTestBase
+{
+    private GetEnlistmentsRequestHandler instance = null!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        instance = new GetEnlistmentsRequestHandler(DbContext);
+    }
+
+    [Test]
+    public async Task Handle_WithNoEnlistments_ReturnsEmptyListAsync()
+    {
+        var response = await instance.Handle(new GetEnlistmentsRequest(), CancellationToken.None);
+
+        Assert.IsEmpty(response.Enlistments);
+    }
+
+    [Test]
+    public async Task Handle_WithMultipleEnlistmentsWithOpenState_ReturnsAllOpenAsync()
+    {
+        var enlistment1 = new Enlistment("testUser1", DateTime.Now);
+        var enlistment2 = new Enlistment("testUser2", DateTime.Now);
+        await AddEnlistmentsAsync(new[]
+        {
+            enlistment1,
+            enlistment2
+        });
+
+        var response = await instance.Handle(new GetEnlistmentsRequest(), CancellationToken.None);
+
+        var collection = response.Enlistments.ToList();
+        Assert.AreEqual(2, collection.Count);
+        Assert.Contains(enlistment1, collection);
+        Assert.Contains(enlistment2, collection);
+    }
+
+    [Test]
+    public async Task Handle_WithClosedRequestAndRequestClosed_ReturnsTheClosedEnlistmentAsync()
+    {
+        var enlistment1 = new Enlistment("testUser1", DateTime.Now);
+        enlistment1.Draw();
+        enlistment1.Close();
+
+        await AddEnlistmentsAsync(new[]
+        {
+            enlistment1,
+        });
+
+        var response = await instance.Handle(
+            new GetEnlistmentsRequest(EnlistmentState.Closed), 
+            CancellationToken.None);
+
+        var collection = response.Enlistments.ToList();
+        Assert.AreEqual(1, collection.Count);
+        Assert.Contains(enlistment1, collection);
+    }
+}
