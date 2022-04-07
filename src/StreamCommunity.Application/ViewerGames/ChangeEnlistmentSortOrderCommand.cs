b76a1b9f89295696392e6c0891eb139dddbb2f35
m4cx <macgyverx83@gmail.com>
2022-04-07 08:16:02 +0200

    feat(sorting): sort enlistment list
    * publish new property for sorting in data model
    * default sort in frontend
    *

--- src/StreamCommunity.Application/ViewerGames/ChangeEnlistmentSortOrderCommand.cs
+++ src/StreamCommunity.Application/ViewerGames/ChangeEnlistmentSortOrderCommand.cs
@@ -1,6 +1,16 @@
-﻿namespace StreamCommunity.Application.ViewerGames;
+﻿using MediatR;
 
-public class ChangeEnlistmentSortOrderCommand
+namespace StreamCommunity.Application.ViewerGames;
+
+public class ChangeEnlistmentSortOrderCommand : IRequest
 {
-    
+    public ChangeEnlistmentSortOrderCommand(int enlistmentId, SortDirection sortDirection)
+    {
+        EnlistmentId = enlistmentId;
+        SortDirection = sortDirection;
+    }
+
+    public int EnlistmentId { get; }
+
+    public SortDirection SortDirection { get; }
 }
